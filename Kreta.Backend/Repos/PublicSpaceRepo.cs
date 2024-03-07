0cacfa03c468e2253bc85c2fcf8ab1182c8ec4e5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-03-07 09:02:19 +0100

    Repó és assambler

---
+++ Kreta.Backend/Repos/PublicSpaceRepo.cs
@@ -0,0 +1,13 @@
+﻿using Kreta.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Repos
+{
+    public class PublicSpaceRepo<TDbContext> : RepositoryBase<TDbContext, PublicSpace>, IPublicSpace
+        where TDbContext : DbContext
+    {
+        public PublicSpaceRepo(IDbContextFactory<TDbContext> dbContextFactory) : base(dbContextFactory)
+        {
+        }
+    }
+}
---
+++ Kreta.Backend/Repos/SchoolClass.cs
@@ -0,0 +1,15 @@
+﻿using Kreta.Backend.Context;
+using Kreta.Shared.Models;
+using Kreta.Shared.Models.SchoolCitizens;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreta.Backend.Repos
+{
+    public class SchoolClass<TDbContext> : RepositoryBase<TDbContext, SchoolClass>, ISchoolClass
+        where TDbContext : DbContext
+    {
+        public SchoolClass(IDbContextFactory<TDbContext> dbContextFactory) : base(dbContextFactory)
+        {
+        }
+    }
+}
