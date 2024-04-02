575b52b7814c5d2db81c031e958289a67ac58e20
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-02 23:02:51 +0200

    SchoolClass - TypeOfEducation desktop kész

--- Kreta.Desktop/ViewModels/Administration/TypeOfEducationViewModel.cs
+++ Kreta.Desktop/ViewModels/Administration/TypeOfEducationViewModel.cs
@@ -15,21 +15,22 @@ namespace Kreta.Desktop.ViewModels.Administration
     public partial class TypeOfEducationViewModel : BaseViewModel
     {
         private readonly ITypeOfEducationService? _typeOfEducationService;
+        private readonly ISchoolClassService? _schoolClassService;
 
         [ObservableProperty]
         private string _title = "Szakmák";
 
         [ObservableProperty]
-        private ObservableCollection<TypeOfEducation> _typeOfEducations = new ObservableCollection<TypeOfEducation>();
+        private ObservableCollection<TypeOfEducation>? _typeOfEducations = new ObservableCollection<TypeOfEducation>();
 
         [ObservableProperty]
-        private ObservableCollection<SchoolClass> schoolClasses = new();
+        private ObservableCollection<SchoolClass>? schoolClasses = new();
 
         [ObservableProperty]
-        private SchoolClass _selectedSchoolClass = new();
+        private SchoolClass? _selectedSchoolClass = new();
 
         [ObservableProperty]
-        private TypeOfEducation _selectedTypeOfEducation=new();
+        private TypeOfEducation? _selectedTypeOfEducation = new();
 
         [ObservableProperty]
         private ObservableCollection<SchoolClass> _schoolClassesWithoutTypeOfEducation = new();
@@ -39,9 +40,12 @@ namespace Kreta.Desktop.ViewModels.Administration
         public TypeOfEducationViewModel()
         {
         }
-        public TypeOfEducationViewModel(ITypeOfEducationService? typeOfEducationService)
+        public TypeOfEducationViewModel(
+            ITypeOfEducationService? typeOfEducationService,
+            ISchoolClassService? schoolClassService)
         {
             _typeOfEducationService = typeOfEducationService;
+            _schoolClassService = schoolClassService;
         }
 
         public async override Task InitializeAsync()
@@ -82,13 +86,57 @@ namespace Kreta.Desktop.ViewModels.Administration
             }
         }
 
+        [RelayCommand]
+        private async Task GetByTypeOfEducationId(Guid typeOfEducationId)
+        {
+            if (_schoolClassService is not null)
+            {
+                List<SchoolClass> schoolClasses = await _schoolClassService.GetByTypeofEducationIdAsync(typeOfEducationId);
+                SchoolClasses = new ObservableCollection<SchoolClass>(schoolClasses);
+                await UpdateView();
+            }
+        }
+
+        [RelayCommand]
+        private async Task MoveSchoolClassToWithoutTypeOfEducation()
+        {
+            if (_schoolClassService is not null && SelectedSchoolClass is not null)
+            {
+                SelectedSchoolClass.TypeOfEducationId = null;
+                ControllerResponse result = await _schoolClassService.UpdateAsync(SelectedSchoolClass);
+                if (result.IsSuccess)
+                    await UpdateView();
+            }
+        }
+
+        [RelayCommand]
+        private async Task MoveToWithTypeOfEducation()
+        {
+            if (_schoolClassService is not null && SelectedTypeOfEducation is not null)
+            {
+                SelectedSchoolClassesWithoutTypeOfEducation.TypeOfEducationId = SelectedTypeOfEducation.Id;
+                ControllerResponse result = await _schoolClassService.UpdateAsync(SelectedSchoolClassesWithoutTypeOfEducation);
+                if (result.IsSuccess)
+                    await UpdateView();
+            }
+        }
 
         private async Task UpdateView()
         {
-            if (_typeOfEducationService is not null)
+            if (_typeOfEducationService is not null && SelectedTypeOfEducation is not null)
             {
+                TypeOfEducation? storedPreviusSelectedTypeOfEducation = SelectedTypeOfEducation is not null ? SelectedTypeOfEducation : new();
+                SelectedTypeOfEducation = null;
                 List<TypeOfEducation> typeOfEducations = await _typeOfEducationService.SelectAllAsync();
                 TypeOfEducations = new ObservableCollection<TypeOfEducation>(typeOfEducations);
+                SelectedTypeOfEducation = typeOfEducations.FirstOrDefault(typeOfEducation => typeOfEducation.Id == storedPreviusSelectedTypeOfEducation.Id);
+                SelectedTypeOfEducation = SelectedTypeOfEducation is not null ? SelectedTypeOfEducation : new();
+            }
+            if (_schoolClassService is not null)
+            {
+
+                List<SchoolClass> schoolClasses = await _schoolClassService.GetSchoolClassesWithoutTypeOfEducation();
+                SchoolClassesWithoutTypeOfEducation = new ObservableCollection<SchoolClass>(schoolClasses);
             }
         }
     }
--- Kreta.HttpService/Services/ISchoolClassService.cs
+++ Kreta.HttpService/Services/ISchoolClassService.cs
@@ -4,5 +4,8 @@ namespace Kreta.HttpService.Services
 {
     public interface ISchoolClassService : IBaseService<SchoolClass>
     {
+        public Task<List<SchoolClass>> SelectAllIncludedAsync();
+        public Task<List<SchoolClass>> GetByTypeofEducationIdAsync(Guid typeOfEducationID);
+        Task<List<SchoolClass>> GetSchoolClassesWithoutTypeOfEducation();
     }
 }
